ee566d1140ea2d97e4304fb19040138e4be27822
T <tobyjonahbee@gmail.com>
2020-03-05 15:55:42 +0000

    Update NewCompanies.sql

--- NewCompanies.sql
+++ NewCompanies.sql
@@ -1,4 +1,9 @@
 /* URL: https://www.hackerrank.com/challenges/the-company/problem */
 
-/* UNFINISHED */
-SELECT C.company_code, C.founder, (SELECT COUNT(lead_manager_code) FROM Lead_Manager LM GROUP BY lead_manager_code WHERE LM.company_code = C.company_code), (SELECT COUNT(senior_manager_code) FROM Senior_Manager SM GROUP BY senior_manager_code WHERE SM.company_code = C.company_code), (SELECT COUNT(manager_code) FROM Manager M GROUP BY manager_code WHERE M.company_code = C.company_code), (SELECT COUNT(employee_code) FROM Employee E GROUP BY employee_code WHERE E.company_code = C.company_code) FROM Company C;
+/* Normal solution: */
+SELECT C.company_code, C.founder,
+(SELECT COUNT(DISTINCT lead_manager_code) FROM Lead_Manager LM WHERE (LM.company_code = C.company_code) GROUP BY LM.company_code) AS leads,
+(SELECT COUNT(DISTINCT senior_manager_code) FROM Senior_Manager SM WHERE (SM.company_code = C.company_code) GROUP BY SM.company_code) AS seniors,
+(SELECT COUNT(DISTINCT manager_code) FROM Manager M WHERE (M.company_code = C.company_code) GROUP BY M.company_code) AS managers,
+(SELECT COUNT(DISTINCT employee_code) FROM Employee E WHERE (E.company_code = C.company_code) GROUP BY E.company_code) AS employees
+FROM Company C ORDER BY C.company_code ASC;
